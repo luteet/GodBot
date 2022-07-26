93ef96feab38805cd0ad70fd91a17be2fd0276cb
Roman Lukjanets <lukyanets14@gmail.com>
2022-07-26 11:17:02 +0300

    v1.0.2

--- app/js/main.js
+++ app/js/main.js
@@ -2,21 +2,65 @@
 const body = document.querySelector('body'),
     html = document.querySelector('html');
 
-/* let thisTarget;
+function validate(email, event) {
+  let reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+  let address = email.value;
+  if(reg.test(address) == false) {
+      event.preventDefault();
+      email.classList.add('_error');
+      email.onblur = function () {
+        email.classList.remove('_error');
+      }
+      return false;
+  }
+}
+
+let thisTarget;
 body.addEventListener('click', function (event) {
 
     thisTarget = event.target;
 
+    let submitBtn = thisTarget.closest('.login__form--submit');
+    if(submitBtn) {
+      
+      if(submitBtn.classList.contains('_disabled')) {
+
+        event.preventDefault();
+
+      } else {
+
+        let form = submitBtn.closest('form'),
+        emailInput = form.querySelector('.email-input');
+
+        if(emailInput) validate(emailInput, event);
+
+      }
+      
+
+    }
+
+})
+
+/* let emailInputs = document.querySelectorAll('.email-input');
+
+emailInputs.forEach(thisInput => {
+  thisInput.oninput = function(event) {
+
+  }
 }) */
+
+
+
 
 let verificationInputs = document.querySelectorAll('.login__verification--input');
 verificationInputs.forEach(thisInput => {
 
   thisInput.oninput = function(event) {
-
+    
     if(event.inputType == 'insertText') {
 
-      let nextElement = event.target.parentElement.nextElementSibling;
+      let nextElement = event.target.parentElement.nextElementSibling,
+          form = event.target.closest('form');
 
       if(nextElement.classList.contains('login__verification--label')) {
         nextElement.querySelector('input').focus();
@@ -24,12 +68,54 @@ verificationInputs.forEach(thisInput => {
 
       thisInput.value = thisInput.value[thisInput.value.length-1];
 
+      if(checkInput()) {
+        form.querySelector('.login__form--submit').classList.add('_disabled')
+      } else {
+        form.querySelector('.login__form--submit').classList.remove('_disabled')
+      }
+
     }
 
   }
 
+  thisInput.addEventListener('keydown', function(event) {
+    
+    if(event.code == 'Backspace' || event.code == 'Delete') {
+      event.preventDefault();
+      
+
+      let prevElement = event.target.parentElement.previousElementSibling,
+      form = event.target.closest('form');
+      
+      if(prevElement) {
+        if(prevElement.classList.contains('login__verification--label') && !event.target.value) {
+          prevElement.querySelector('input').focus();
+        } else {
+          event.target.value = '';
+        }
+      } else {
+        event.target.value = '';
+      }
+      
+      if(checkInput()) {
+        form.querySelector('.login__form--submit').classList.add('_disabled')
+      } else {
+        form.querySelector('.login__form--submit').classList.remove('_disabled')
+      }
+      
+    }
+    
+  })
+
 })
 
+function checkInput() {
+  for(let index = 0; index < verificationInputs.length; index++) {
+    if(!verificationInputs[index].value) {
+      return true;
+    }
+  }
+}
 
 function timer() {
   const timerElems = document.querySelectorAll('._timer');
@@ -46,6 +132,8 @@ function timer() {
       if(seconds == 0) {
         clearTimeout(timerInterval);
         thisTimerElem.parentElement.classList.remove('_disabled');
+        thisTimerElem.style.display = 'none';
+        thisTimerElem.nextElementSibling.style.display = 'block';
       } else {
         seconds--;
       }
